375c12da250bc7d595602bd7d59f73d81f8aa1a2
Tong Lin <38441172+GermanShepherd13@users.noreply.github.com>
2021-12-09 15:24:18 -0800

    idk

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,7 +1,8 @@
 package frc.robot.subsystems;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
 
 public class Intake extends SubsystemBase {
   /**
@@ -14,14 +15,9 @@ public class Intake extends SubsystemBase {
 // one motor
 //permanently run motor, it spins rod
 
-WPI_TalonFX m_main = new WPI_TalonFX(0);
 // initializes new motor
-
+WPI_TalonFX motor = new WPI_TalonFX(0);
   @Override
-
-  
-
-  
   public void periodic() { //test comment
 
     // This method will be called once per scheduler run
